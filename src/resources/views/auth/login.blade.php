a5158378fcfd4b948b3e32a49dd1a1c674eff580
hisano satomi <stm1014@yahoo.ne.jp>
2025-08-24 08:29:23 +0900

    ログイン画面のスタイル調整

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
     <title>coachtechフリマ</title>
     
 </head>
@@ -37,7 +39,7 @@
         </form>
 
         <div class="register-link">
-            <a href="{{ route('register') }}">会員登録はこちら</a>
+            <a class="register-link--blue" href="{{ route('register') }}">会員登録はこちら</a>
         </div>
     </main>
 </body>
